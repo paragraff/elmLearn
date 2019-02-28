9d4f9b9073c0b3a2f3f5178a941e037d0af4a5b0
Oleksandr Kovtiukh <a.kovtukh@gmail.com>
2019-02-28 10:29:32 +0200

    init

---
+++ src/1.elm
@@ -0,0 +1,40 @@
+--1
+-- List.reverse list |> list.head
+-- list = List.foldl (Just >> always) Nothing list
+myLast list = List.foldl (\var arr -> Just var) Nothing list
+
+--3
+elementAt at list = if List.length list < at then Nothing else List.take at list |> List.reverse |> List.head
+
+--2
+myButLast list = elementAt ((List.length list) - 1) list
+
+--4
+myLength list = List.foldl (\num result -> result + 1) 0 list
+
+--5
+myReverse list =  List.foldl (\num arr -> num :: arr) [] list
+
+--6
+isPalindrome list = List.all (\compare -> compare) (List.map2 (\x y -> x == y) list (myReverse list))
+
+--7
+compress str = \
+  let removeRepetitions chr result = if List.head result /= Just chr then chr :: result else result \
+  in List.foldl removeRepetitions [] (String.toList str) |> String.fromList |> String.reverse \
+
+--8
+dropEvery str dropNum = \
+  let filterDropNum value = modBy dropNum (Tuple.first value + 1) /= 0 \
+      getChar value = Tuple.second value \
+  in if dropNum /= 0 then \
+   String.toList str \
+    |> List.indexedMap Tuple.pair \
+    |> List.filter filterDropNum \
+    |> List.map getChar \
+    |> String.fromList \
+  else \
+    ""
+
+--9
+clap str = str |> String.words |> String.join " 👏 "
